7d2d12a44495a1f6a14860c8f8b87c9766c2c2f2
Tanmay Pandey <36691630+tanmaypandey7@users.noreply.github.com>
2019-01-12 21:35:59 +0530

    added directories

--- assignment.py
+++ assignment.py
@@ -1,7 +1,7 @@
 import pydicom
 
-dcm1=r"C:\Users\Vision\Desktop\Digisoft-Assignment\ttfm.dcm"
-dcm2=r"C:\Users\Vision\Desktop\Digisoft-Assignment\bmode.dcm"
+dcm1=r"dcm files\bmode.dcm"
+dcm2=r"dcm files\ttfm.dcm"
 
 dcm_data1 = pydicom.dcmread(dcm1)
 dcm_data2 = pydicom.dcmread(dcm2)
@@ -9,8 +9,8 @@ dcm_data2 = pydicom.dcmread(dcm2)
 temp1=str(dcm_data1)
 temp2=str(dcm_data2)
 
-with open("ttfm.txt", "w") as text_file:
+with open(r"text files\bmode.txt", "w") as text_file:
     text_file.write(temp1)
 
-with open("bmode.txt", "w") as text_file:
+with open(r"text files\ttfm.txt", "w") as text_file:
     text_file.write(temp2)
